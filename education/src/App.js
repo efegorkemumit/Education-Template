e6308cbf1407c9e169974fe0c3fd5d71f7b06f2d
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-26 00:34:12 +0300

    TailwindCss Ozelleştirme

--- education/src/App.js
+++ education/src/App.js
@@ -4,20 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-red-700'>as
-       
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <span> 
+        <h2 className='font-poppins'>Test</h2>
+        <h2 className='font-roboto'>Test</h2>
+        <h2 className='font-lora'>Test</h2>
+      </span>
+
+      <div className='bg-primary-light'>
+
+        testtt
+      </div>
+
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+  <div>
+    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+    </span>
+  </div>
+  <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+  <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+  </p>
+</div>
+      
     </div>
   );
 }
--- education/tailwind.config.js
+++ education/tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+      'sm':'576px',
+      'md':'768px',
+      'lg':'920px',
+      'xl':'1198px',
+      '2xl':'1536px',
+
+    },
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins'],
+        'roboto':['Roboto'],
+        'lora':['Lora']
+      },
+
+      colors:{
+        primary:{
+          light:'#fff',
+          dark:'#000'
+        }
+      }
+    },
   },
   plugins: [],
 }
