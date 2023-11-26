80e977bf5c5d4d3ea96bde7ee6fa5d8e66b3659b
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-26 11:43:51 +0300

    SwiperSlide
    Youtube Efe Görkem Ümit

--- education/src/App.js
+++ education/src/App.js
@@ -1,33 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      
-      <span> 
-        <h2 className='font-poppins'>Test</h2>
-        <h2 className='font-roboto'>Test</h2>
-        <h2 className='font-lora'>Test</h2>
-      </span>
-
-      <div className='bg-primary-light'>
-
-        testtt
-      </div>
-
-      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-  <div>
-    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
-    </span>
-  </div>
-  <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-  <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
-</div>
-      
+    <div >
+      <Header></Header>
+      a
     </div>
   );
 }
