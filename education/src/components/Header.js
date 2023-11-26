26085f7c3d73611067f85f3804a48baeea7bcacb
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-26 17:32:51 +0300

    Slider

--- education/src/components/Header.js
+++ education/src/components/Header.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../myswiper.css';
+
 
 function Header() {
   return (
     <div>
 
 <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src='img/slider/1.jpg'></img>
+
+
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src='img/slider/2.jpg'></img>
+
+
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src='img/slider/3.jpg'></img>
+
+
+        </SwiperSlide>
+       
+      </Swiper>
     </div>
   )
 }
