ba86609270564f0e6291519c688e2d8dae762d17
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-27 02:01:13 +0300

    Darkmode-Light Mode

--- education/src/components/Header.js
+++ education/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
@@ -8,6 +8,25 @@ import '../myswiper.css';
 
 
 function Header() {
+
+  const [theme, setTheme] =useState(
+    localStorage.theme === 'dark' || (!('theme' in localStorage) && 
+      window.matchMedia('(prefers-color-scheme:dark)').matches) ?'dark' : 'light'
+
+
+  );
+
+  const toogleTheme = (selectedTheme)=>{
+    setTheme(selectedTheme);
+    localStorage.theme=selectedTheme;
+
+  }
+
+  useEffect(()=>{
+
+    document.documentElement.classList.toggle('dark', theme==='dark');
+
+  },[theme]);
   return (
     <div className='relative'>
       <header className='absolute inset-x-0 top-0 z-50 py-0'>
@@ -27,17 +46,26 @@ function Header() {
                </div>
 
                <div className='flex items-center space-x-4'>
-                     <a href='#' className='text-white text-xl hover:underline transition '>Login</a>
+                     <a href='#' className='text-white text-xl hover:underline transition  '>Login</a>
 
-                     <a href='#' className='text-white text-xl '>
-                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                     <a onClick={()=>toogleTheme('dark')} className={`text-white text-xl ${theme=='dark'? 'hidden':'block'}`}>
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="cursor-pointer w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
 </svg></a>
 
-<a href='#' className='text-white text-xl lg:hidden flex'>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+<a onClick={()=>toogleTheme('light')}  className={`text-white text-xl ${theme=='light'? 'hidden':'block'}`}>
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" cursor-pointer w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
 </svg>
+
+</a>
+
+<a href='#' className='text-white text-xl lg:hidden flex'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+</svg>
+
 
                    
   </a>
@@ -68,7 +96,7 @@ function Header() {
           <section className='relative h-full overflow-hidden'>
             <div className='absolute top-0 left-0 w-full h-full bg-cover bg-center'  style={{backgroundImage:'url("img/slider/1.jpg")'}}></div>
             <div className='absolute top-0 left-0 w-full h-full bg-opacity-40 bg-black'></div>
-            <div className='hidden absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black'></div>
+            <div className='hidden dark:block absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></div>
             <div className='flex justify-between items-center py-28 px-5 relative z-10'>
               <div className='w-1/2 space-y-6'>
                 <h2 className='uppercase text-3xl lg:text-6xl font-poppins font-semibold text-white'>lorem ipmsum title</h2>
@@ -97,7 +125,7 @@ function Header() {
           <section className='relative h-full overflow-hidden'>
             <div className='absolute top-0 left-0 w-full h-full bg-cover bg-center'  style={{backgroundImage:'url("img/slider/2.jpg")'}}></div>
             <div className='absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black'></div>
-            <div className='hidden absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black'></div>
+            <div className='hidden dark:block absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></div>
             <div className='flex justify-between items-center py-28 px-5 relative z-10'>
             <div className='w-1/2 space-y-6'>
                 <h2 className='uppercase text-3xl lg:text-6xl font-poppins font-semibold text-white'>lorem ipmsum title</h2>
@@ -126,7 +154,7 @@ function Header() {
           <section className='relative h-full overflow-hidden'>
             <div className='absolute top-0 left-0 w-full h-full bg-cover bg-center'  style={{backgroundImage:'url("img/slider/3.jpg")'}}></div>
             <div className='absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black'></div>
-            <div className='hidden absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black'></div>
+            <div className='hidden dark:block absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></div>
             <div className='flex justify-between items-center py-28 px-5 relative z-10'>
             <div className='w-1/2 space-y-6'>
                 <h2 className='uppercase text-3xl lg:text-6xl font-poppins font-semibold text-white'>lorem ipmsum title</h2>
