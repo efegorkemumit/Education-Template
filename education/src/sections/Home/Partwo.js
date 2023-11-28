35c91a7c423711b678a3b535b250434bd180fb45
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-28 21:40:01 +0300

    Partwo 1

--- education/src/sections/Home/Partwo.js
+++ education/src/sections/Home/Partwo.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 function Partwo() {
   return (
-    <div>Partwo</div>
+    <div>
+        <section className='relative'>
+            <div className='relative pt-52 pb-32 flex content-center items-center justify-center'>
+                <div className='absolute top-0 w-full h-full bg-cover bg-center' style={{backgroundImage:'url("img/slider/slider2.jpg")'}}>
+                <span className='absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></span>
+                <span className='hidden dark:block absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></span>
+
+                <div className='container relative mx-auto'>
+                    <div className='items-center flex flex-wrap'>
+                        <div className='w-full lg:w-6-/12 px-4  ml-auto mr-auto text-center  pt-32'>
+
+                            <h1 className='text-white font-semibold text-5xl'>lorem ipsum</h1>
+
+                            <p className='mt-4 text-lg text-white' >
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            </p>
+
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+        </section>
+
+
+        
+    </div>
   )
 }
 
