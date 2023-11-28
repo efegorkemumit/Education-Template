b08c880735b820c0a4f71261054d766a90e4ef25
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-28 21:26:07 +0300

    Hover animation

--- education/src/screens/HomeScreen.js
+++ education/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ServiceSection from '../sections/Home/ServiceSection'
+import Partwo from '../sections/Home/Partwo'
 
 function HomeScreen() {
   return (
@@ -15,6 +16,7 @@ function HomeScreen() {
 
         </div>
         <ServiceSection></ServiceSection>
+        <Partwo></Partwo>
 
         <Footer/>
     </div>
---
+++ education/src/sections/Home/Partwo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Partwo() {
+  return (
+    <div>Partwo</div>
+  )
+}
+
+export default Partwo
