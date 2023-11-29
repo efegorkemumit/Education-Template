c1d6b2e14b46feee76a10e92a7fa5c759fe43fc9
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-29 19:01:08 +0300

    Product Slider Dark Mode

--- education/src/sections/Home/ProductSection.js
+++ education/src/sections/Home/ProductSection.js
@@ -33,6 +33,7 @@ function ProductSection() {
         className="mySwiper"
       >
 
+
         {productData.map((product)=>(
 
         <SwiperSlide key={product.id}>
@@ -41,12 +42,46 @@ function ProductSection() {
         </SwiperSlide>
 
         ))}
-
         
+        
+
       
        
       </Swiper>
+      <section className='relative'>
+<div className='relative pt-72 pb-32 flex content-center items-center justify-center'>
+                <div className='absolute top-0 w-full h-full bg-cover bg-center' style={{backgroundImage:'url("img/slider/slider3.jpg")'}}>
+                <span className='absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></span>
+                <span className='hidden dark:block absolute top-0 left-0 w-full h-full bg-opacity-70 bg-black'></span>
 
+                <div className='container relative mx-auto'>
+                    <div className='items-center flex flex-wrap'>
+                        <div className='w-full lg:w-6-/12 px-4  ml-auto mr-auto text-center  pt-32'>
+
+                            <h1 className='text-white font-semibold text-5xl title-animation'>lorem ipsum</h1>
+
+                            <p className='mt-4 text-lg text-white' >
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            </p>
+
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+           </section>
 
         <section className='py-10 bg-white dark:bg-primary-dark'>
 
