f40ecee05408e85fab3e869df63653e833f94e66
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-30 00:21:50 +0300

    Mobile Menu 2

--- education/src/components/Menu.js
+++ education/src/components/Menu.js
@@ -20,13 +20,19 @@ function Menu() {
         document.documentElement.classList.toggle('dark', theme==='dark');
       },[theme]);
 
+      const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false)
+
+      const toggleMobileMenu=()=>{
+        setisMobileMenuOpen(!isMobileMenuOpen);
+      }
+
   return (
     <div>
-
+{isMobileMenuOpen &&( 
       <div className='fixed left-0 top-0 w-full h-full z-50 bg-black bg-opacity-30 shadow'>
         <div className='absolute left-0 top-0 w-72 h-full z-50 bg-primary-light dark:bg-primary-dark text-second-light dark:text-second-dark'>
 
-<div className='absolute right-3 top-3 cursor-pointer'>
+<div onClick={toggleMobileMenu} className='absolute right-3 top-3 cursor-pointer'>
 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -50,7 +56,7 @@ function Menu() {
 
 
       </div>
-
+)}
 <div className='container mx-auto flex items-center justify-between px-4'>
                <div className=' text-white text-xl  transition font-bold'>Logo</div>
 
@@ -80,7 +86,7 @@ function Menu() {
 
 </a>
 
-<a href='#' className='text-white text-xl lg:hidden flex'>
+<a  onClick={toggleMobileMenu} className='text-white text-xl lg:hidden flex'>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
 </svg>
