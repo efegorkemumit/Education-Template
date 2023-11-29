8a2d2a439fd59dcb1a7791f5476f36ab60b1606f
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-29 22:36:27 +0300

    Footer

--- education/src/components/Footer.js
+++ education/src/components/Footer.js
@@ -2,7 +2,68 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+
+    
+    <div className='bg-primary-light dark:bg-primary-dark text-white dark:text-second-dark  shadow-2xl'>
+     <div className='bg-second-light text-black p-1 bg-opacity-50 text-center'>
+
+        </div>
+    <div className='container mx-auto p-8 '>
+      <footer className='flex flex-wrap '>
+
+<div className='w-full lg:w-1/4 p-6'>
+  <h3 className='text-lg font-semibold mb-4 font-lora'>About us</h3>
+  <p>Lorem Ipsum t to make a type specimen book typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+</p>
+
+</div>
+
+<div className='w-full lg:w-1/4 p-6'>
+  <h3 className='text-lg font-semibold mb-4 font-lora'>Contact</h3>
+
+<p>Address :  Losangeles California Turkey Bişey Street</p>
+<p>Youtubeabone@gmail.com</p>
+<p>0535 111 222 33</p>
+</div>
+
+<div className='w-full lg:w-1/4 p-6'>
+  <h3 className='text-lg font-semibold mb-4 font-lora'>About us</h3>
+
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+
+</div>
+
+
+<div className='w-full lg:w-1/4 p-6'>
+  <h3 className='text-lg font-semibold mb-4 font-lora'>About us</h3>
+
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+<li className='mb-2'>Contact</li>
+
+</div>
+
+
+      </footer>
+
+
+
+    </div>
+
+    <div className='bg-second-light text-black p-3 text-center'>
+          <p className='text-md font-semibold'>🎄 is simply dummy text of the printing and typesetting industry. Lorem</p>
+
+        </div>
+    </div>
+
+    
   )
 }
 
